4a41359ab706a0700921b4c1c2ce3a48d8f38f81
darthenis <emi.acevedo.letras@gmail.com>
2022-03-02 19:09:20 -0300

    asd

--- src/app/services/interceptor-http-error.service.ts
+++ src/app/services/interceptor-http-error.service.ts
@@ -17,7 +17,7 @@ export class InterceptorHttpErrorService implements HttpInterceptor{
 
           if (error.error instanceof ErrorEvent) {
             // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error);
+            console.error('An error occurred:', error.error.message);
           } else {
             // The backend returned an unsuccessful response code.
             // The response body may contain clues as to what went wrong,
@@ -25,7 +25,7 @@ export class InterceptorHttpErrorService implements HttpInterceptor{
               `Backend returned code ${error.status}, ` +
               `body was: ${error.error}`);
           }
-        });
+        })
 
       })
     )
